6f19415eb1300ce7dab01e2119313ea82e39be29
Daria Nowak <dafka_2703@mail.ru>
2024-10-07 17:49:26 +0300

    1

--- src/js/buttonBrands.js
+++ src/js/buttonBrands.js
@@ -2,19 +2,21 @@ document.addEventListener('DOMContentLoaded', function () {
   const button = document.querySelector('.btn-more');
   const brend_list = document.querySelector('.brands-list');
   const button_text = button.querySelector('span');
-  const hidden = brend_list.querySelectorAll('#hidden');
+  //const hidden = brend_list.querySelectorAll('#hidden');
 
   button.addEventListener('click', function () {
     //hidden.forEach((element) => {
     //  element.classList.toggle('brands-list__item--hidden');
     //});
-    const section = document.getElementsByClassName('brands swiper');
-    //section.style.height = 100;
-    section.style.height = 'fit-content';
+    const section = document.getElementById('hiding_list');
     button.classList.toggle('btn-more--rotate');
-    button_text.innerHTML =
-      button_text.innerHTML === 'Скрыть'
+    let isOpened = button_text.innerHTML === 'Скрыть';
+    section.style.height = isOpened
+    ? '222px'
+    : 'fit-content';
+
+    button_text.innerHTML = isOpened
         ? 'Показать все'
-        : 'Скрыть';
+        : 'Скрыть';    
   });
 });
