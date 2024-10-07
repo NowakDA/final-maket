a358a12502237f34bee7f76f462b3abe712c8ac5
Daria Nowak <dafka_2703@mail.ru>
2024-10-07 14:21:37 +0300

    1

--- src/js/buttonBrands.js
+++ src/js/buttonBrands.js
@@ -8,6 +8,9 @@ document.addEventListener('DOMContentLoaded', function () {
     hidden.forEach((element) => {
       element.classList.toggle('brands-list__item--hidden');
     });
+    //const section = document.getElementsByClassName('brands swiper');
+    //section.style.height = 100;
+    //section.style.height = 'fit-content';
     button.classList.toggle('btn-more--rotate');
     button_text.innerHTML =
       button_text.innerHTML === 'Скрыть'
